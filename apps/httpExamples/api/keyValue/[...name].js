25ba965a4476cdebc0b3f830995e37e87cbe6ce9
Jeff McAffer <jeffmcaffer@github.com>
2022-03-16 22:37:26 +0000

    update for new syntax/structure

--- apps/httpExamples/api/keyValue/[...name].js
+++ apps/httpExamples/api/keyValue/[...name].js
@@ -1,4 +1,4 @@
-import { keyValue } from '@extendohub/runtime'
+import { kv } from '@extendohub/storage'
 
 export default async ({ api }) => {
   const { request, response } = api
@@ -10,15 +10,15 @@ const handlers = {
   get: async (request, response) => {
     const { resource, params, query } = request
     const { name } = params
-    const current = await keyValue.get(name)
+    const current = await kv.get(name)
     log.info(`Hello ${name}(${query.who}) from ${resource.typeName} ${resource.name}\nThe value at "${name}" is:\n${JSON.stringify(current)}\n`)
     return response.json(current)
   },
   post: async (request, response) => {
     const { resource, params, query, body } = request
     const { name } = params
-    const old = await keyValue.get(name)
-    await keyValue.set(name, body)
+    const old = await kv.get(name)
+    await kv.set(name, body)
     log.info(`Hello ${name}(${query.who}) from ${resource.typeName} ${resource.name}\nSetting value at "${name}" to: ${JSON.stringify(body)}`)
     return response.json(old)
   }
